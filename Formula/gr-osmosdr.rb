5ccfc2d1071f9189f00fbcc8f1c1a6c926c96ed0
michael tesch <tesch1@gmail.com>
2023-03-15 04:10:43 -0500

    fix

--- Formula/gr-osmosdr.rb
+++ Formula/gr-osmosdr.rb
@@ -3,14 +3,14 @@ class GrOsmosdr < Formula
   homepage "https://osmocom.org/projects/gr-osmosdr/wiki/GrOsmoSDR"
   url "https://github.com/osmocom/gr-osmosdr/archive/refs/tags/v0.2.4.tar.gz"
   sha256 "28b6f2768aee7b397b227e9e70822e28de3b4c5362a5d14646a0948a48094a63"
-  license "GPL-3"
+  license "GPL-3.0-or-later"
 
-  depends_on "cmake" => :build
   depends_on "boost" => :build
+  depends_on "cmake" => :build
   depends_on "gnuradio"
-  depends_on "soapysdr"
-  depends_on "pybind11"
   depends_on "libsndfile"
+  depends_on "pybind11"
+  depends_on "soapysdr"
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DENABLE_NONFREE=TRUE"
